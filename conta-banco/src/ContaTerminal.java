39b8d5d465656a022d1a6bb3755a2c6ec8b7e6b0
Vinicius Galvão <vinicius.galvao@impa.br>
2025-04-10 21:20:45 -0300

    entrada de dados de conta de um cliente

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in);
+
+        String agencia = "";
+        int numero = 0;
+        String nomeCliente = "";
+        double saldo = 0;
+       
+        System.out.print("Digite seu nome: ");
+        nomeCliente = scanner.nextLine();
+
+        System.out.print("Digite o número da agência: ");
+        agencia = scanner.nextLine();
+
+        System.out.print("Digite o número da conta: ");
+        numero = scanner.nextInt();
+
+        System.out.print("Digite seu saldo: ");
+        saldo = scanner.nextDouble();
+        
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " +
+        agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+
+        scanner.close();
     }
 }
